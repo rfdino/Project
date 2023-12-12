23335677a62f64c39769568364bb4203544a7d19
Rafael <rm54829p@pace.edu>
2023-12-12 13:45:36 -0500

    rt

--- MainCode.py
+++ MainCode.py
@@ -3,17 +3,57 @@ class char:
         self.name = n
         self.spec = ""
         self.clas = ""
+        self.back = ""
+        self.level = 0
+        self.stats = {}
+    def chooseBackground(self,b):
+        self.back = b
+    def getBackground(self):
+        return self.back
+    def getClass(self):
+        return self.clas
     def chooseClass(self,an):
-        ae = an.lower()
-        self.clas = ae
+        self.clas = an
+    def chooseSpec(self,sp):
+        self.spec = sp
+    def chooseLevel(self,l):
+        self.level = 1
+    def chooseStats(self):
+        emptStat ={"strength":0,"health":0,"defence":0,"magic":0,"intellegence":0,"speed":0}
+        maxStat = 10*self.level
+        while maxStat > 0:
+            ras = input("Which stat do you wish to increase?\nStrength? Health?\n You have {} points left")
+            ras = ras.lower()
+            if ras != "strength" or ras != "health" or ras!= "defence" or ras != "magic"or ras !="intellegence"or ras !="speed":
+                while ras != "strength" or ras != "health" or ras!= "defence" or ras != "magic"or ras !="intellegence"or ras !="speed":
+                    ras= input("Typo or not an option please type again")
+            nu = input("Incease stat by how much?")
+            if nu.isdigit() == False:
+                while nu.isdigit() == False:
+                    nu = input("Not a number please enter again")
+            if nu > maxStat:
+                while nu > maxStat and nu.isdigit == False:
+                    nu = input("You don't have enoght points left for that, try entring another number")
+            emptStat[ras]+=nu
+            maxStat = maxStat - nu
+
+
+    def __str__(self):
+        return "{} is a {} {} from".format(self.name,self.spec,self.clas)
+
 def makeChar():
     na = input("Choose a name")
     newchar = char(na)
     cla = input("Choose a class out of these options\nWarrior Cleric Mage Chef Theif Engineer Tamer Ranger or Custom")
-
+    lev = int(input("What level do you want your character to be?"))
+    if lev > 100 or lev<0:
+        while lev > 100 or lev<0:
+            lev = int(input("Please enter a level between 1 and 100"))
+    newchar.chooseLevel(lev)
+    newchar.chooseStats()
 
 def main():
-    ans = input("What would you like to do?\nMake character?  View charcter?  Compare stats?  Equip items?")
+    ans = input("What would you like to do?\nMake character?  View charcter?  Compare stats?  Equip items? Quit")
     makeChar()
 
 if __name__ == '__main__':
