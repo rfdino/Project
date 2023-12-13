042a0674d1cc4d0d0985a8484f822a4cf1404a05
Rafael <rm54829p@pace.edu>
2023-12-13 16:50:34 -0500

    fg

--- MainCode.py
+++ MainCode.py
@@ -1,3 +1,4 @@
+import _json
 class char:
     def __init__(self,n):
         self.name = n
@@ -22,39 +23,57 @@ class char:
         emptStat ={"strength":0,"health":0,"defence":0,"magic":0,"intellegence":0,"speed":0}
         maxStat = 10*self.level
         while maxStat > 0:
+            print("{}{}{}".format(emptStat["strength"],emptStat["health"],emptStat["defence"]))
             ras = input("Which stat do you wish to increase?\nStrength? Health?\n You have {} points left")
             ras = ras.lower()
-            if ras != "strength" or ras != "health" or ras!= "defence" or ras != "magic"or ras !="intellegence"or ras !="speed":
-                while ras != "strength" or ras != "health" or ras!= "defence" or ras != "magic"or ras !="intellegence"or ras !="speed":
-                    ras= input("Typo or not an option please type again")
-            nu = input("Incease stat by how much?")
-            if nu.isdigit() == False:
-                while nu.isdigit() == False:
-                    nu = input("Not a number please enter again")
-            if nu > maxStat:
-                while nu > maxStat and nu.isdigit == False:
-                    nu = input("You don't have enoght points left for that, try entring another number")
-            emptStat[ras]+=nu
-            maxStat = maxStat - nu
-
+            if ras == "strength" or ras == "health" or ras == "defence" or ras == "magic"or ras =="intellegence"or ras =="speed":
+                nu = input("Incease stat by how much?")
+                if nu.isdigit() == False:
+                    while nu.isdigit() == False:
+                       nu = input("Not a number please enter again")
+                    if nu > maxStat:
+                        while nu > maxStat and nu.isdigit == False:
+                            nu = input("You don't have enoght points left for that, try entring another number")
+                    emptStat[ras]+=nu
+                    maxStat = maxStat - nu
 
     def __str__(self):
-        return "{} is a {} {} from".format(self.name,self.spec,self.clas)
+        return "{},{},{},".format(self.name,self.spec,self.clas)
 
 def makeChar():
     na = input("Choose a name")
     newchar = char(na)
     cla = input("Choose a class out of these options\nWarrior Cleric Mage Chef Theif Engineer Tamer Ranger or Custom")
+    if cla == "custom":
+        newchar.chooseClass(cla)
     lev = int(input("What level do you want your character to be?"))
     if lev > 100 or lev<0:
         while lev > 100 or lev<0:
             lev = int(input("Please enter a level between 1 and 100"))
     newchar.chooseLevel(lev)
     newchar.chooseStats()
+def viewChar():
+    na = input("Enter the characters name")
+    open(na,"r")
+def compare():
+    com1 = input("How do want to choose")
+    com2 = input("Who do you want to compare to")
+def item():
+    eo = input("")
+
 
 def main():
     ans = input("What would you like to do?\nMake character?  View charcter?  Compare stats?  Equip items? Quit")
-    makeChar()
+    if ans == "make character":
+        makeChar()
+    elif ans == "view character":
+        viewChar()
+    elif ans == "compare stats":
+        compare()
+    elif ans == "equip":
+        item()
+    elif ans == "quit":
+        exit()
 
 if __name__ == '__main__':
     main()
